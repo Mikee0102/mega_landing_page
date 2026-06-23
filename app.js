08d556f4a24513dbacddf1ef9db8b0cc843070e8
Mike <mv236648@gmail.com>
2026-06-23 13:21:19 -0600

    Agregando un job cono js

---
+++ app.js
@@ -0,0 +1,8 @@
+// imprimir mensaje en consola
+console.log("Hola a todos soy Mike");
+
+//Imprimir figura en consola
+console.log("     👻 ");
+console.log("   👻👻👻 ");
+console.log("  👻👻👻👻 ");
+console.log("👻👻👻👻👻👻");
